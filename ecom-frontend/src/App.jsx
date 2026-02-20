f1e0995524c00bb8b215861c62ff80e54d949f05
Naran khadka <khadkan855@gmail.com>
2026-02-20 16:40:55 +0545

    Start frontEnd

---
+++ ecom-frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
+import Products from './components/Products';
+
+
+function App() {
+  return (
+    <div>
+
+      <Products/>
+
+    </div>
+    
+  );
+}
+
+export default App;
+
---
+++ ecom-frontend/src/components/ProductCard.jsx
@@ -0,0 +1,9 @@
+const ProductCart = () => 
+{
+    return(
+        <div>
+            <h1>Products</h1>
+        </div>
+    )
+}
+export default ProductCart;
